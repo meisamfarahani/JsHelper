baeac6c14ed3495a4601532f1f80c8839e53e13c
meisamfarahani <87523209+meisamfarahani@users.noreply.github.com>
2021-07-16 14:12:54 +0430

    Update jsHelper.js

--- jsHelper.js
+++ jsHelper.js
@@ -6,6 +6,10 @@
  
  var jsHelper = {
  
-  
+  var replaceAllString = function(string, Find, Replace) {
+    try {
+        return string.replace(new RegExp(Find, "gi"), Replace);
+    } catch (ex) { return string; }
+  };
  
  };
